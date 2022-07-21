6811ea4d11ec62849562f713755e95fbb2c1466e
FajarRhamdhani <rhamdhani.fajar3@gmail.com>
2022-07-21 20:43:15 +0700

    tugas update function prima

--- tugasjs1.js
+++ tugasjs1.js
@@ -4,17 +4,19 @@ var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
 //var arr = [10, 8, 15, 20, 1, 5, 7, 9, 1, 2, 25, 30, 8, 11, 35, 8, 40, 33];
 var arrnew =[]; 
 
+function prima () {
 for (let i = 0; i < arr.length; i++) {   
     //console.log(arr[i])
-    var counter = 0;
-     for (let j = 0; j < arr.length; j++) {
-             if(arr[i] % arr[j] === 0 && arr[i] % 1 === 0){
-                 counter+=1
-             }
-     }
-     if(counter <= 2){
-        arrnew.push(arr[i]);
-     }
-     
+        var counter = 0;
+        for (let j = 0; j < arr.length; j++) {
+                if(arr[i] % arr[j] === 0 && arr[i] % 1 === 0){
+                    counter+=1
+                }
+        }
+        if(counter <= 2){
+            arrnew.push(arr[i]);
+        }
+    }    
+    return arrnew
 }
-console.log(arrnew);
+console.log(prima());
